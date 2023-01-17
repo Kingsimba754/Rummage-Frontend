510fb743eed08410510bac97b060571a88f68805
Lehi Cano <onacihel@gmail.com>
2023-01-17 15:08:25 -0500

    wip

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,20 +7,9 @@ const Main = (props) => {
     <div className="main">
       <div className="main-wrap">
       <div className="welcome">
-        <span className="big">Welcome</span> to Rummage. A place to rid yourself of unwanted things, pick up cool stuff for cheap, or browse for things you didn't know you wanted/needed. 
+        <span className="big">Welcome to Rummage!</span> A place to rid yourself of unwanted things, pick up cool stuff for cheap, or browse for things you didn't know you wanted/needed. 
       </div>
       <div className="item-cards">
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
         <Card/>
       </div>
       <NewForm/>
