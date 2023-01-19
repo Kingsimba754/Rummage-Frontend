11ca51eda2153537a1399a97eb50c971f7e7284d
Lehi Cano <onacihel@gmail.com>
2023-01-19 12:49:54 -0500

    wip

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -15,13 +15,11 @@ function Show(props) {
     
     const loaded = () =>{
         return (
-            <div className="item">
+            <div className="item-show">
                 <h2>{item.name}</h2>
                 <h2>{item.image && <img src={item.image} alt = {item.name}/>}</h2>
                 <h3>{item.description}</h3>
-                {/* <form onSubmit = {handleSubmit}>
-
-                </form> */}
+                <h4>{item.price}</h4>
                 <button onClick = {handleDelete}>Delete this Item</button>
             </div>
         );
