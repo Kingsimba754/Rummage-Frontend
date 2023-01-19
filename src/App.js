c6f72930f15265c443e62454307d494c109515d0
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-01-19 14:34:23 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 import './App.css';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <Header/>
     <Main/>
+    <Footer/>
     </div>
   );
 }
